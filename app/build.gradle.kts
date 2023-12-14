07e594b611b550ee9aa67874085ddf1fb2683270
hegunhee <gunhee0072@gmail.com>
2023-12-14 23:38:26 +0900

    [build-setting] build : toml 파일 작성, app모듈의 gradle 파일 version catalog적용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,19 +51,23 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+
+    implementation(libs.bundles.compose.ui)
+    implementation(libs.material3)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom.test))
+    androidTestImplementation(libs.compose.junit)
+
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
 }
