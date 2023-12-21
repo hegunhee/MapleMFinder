2d5b9be32bfadddbe79b349801cf5e03b7533fe3
hegunhee <gunhee0072@gmail.com>
2023-12-21 22:28:10 +0900

    [nexon-api] test : BuildConfig가 잘 작동하는지 테스트

--- core/data/src/test/java/com/hegunhee/maplemfinder/core/data/ExampleUnitTest.kt
+++ core/data/src/test/java/com/hegunhee/maplemfinder/core/data/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun testApiKey() {
+        val apiKey = BuildConfig.API_KEY
+        assertEquals("Insert Your Api Key",apiKey)
+    }
 }
