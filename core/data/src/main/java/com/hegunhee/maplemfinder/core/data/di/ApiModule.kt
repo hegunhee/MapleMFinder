ed2695db77cfc06a4a1142c1d2bbb5d80124039f
hegunhee <gunhee0072@gmail.com>
2023-12-22 09:16:18 +0900

    [nexon-api] feat : MapleM Api를 위한 ApiModule 작성, Ocid를 가져오는 query 작성

---
+++ core/data/src/main/java/com/hegunhee/maplemfinder/core/data/di/ApiModule.kt
@@ -0,0 +1,59 @@
+package com.hegunhee.maplemfinder.core.data.di
+
+import com.hegunhee.maplemfinder.core.data.BuildConfig
+import com.hegunhee.maplemfinder.core.data.api.MapleMApi
+import com.hegunhee.maplemfinder.core.data.util.MapleMMoshiName
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class ApiModule {
+
+    @Singleton
+    @Provides
+    @Named(MapleMMoshiName)
+    fun provideMapleMMoshi() : Moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Singleton
+    @Provides
+    fun provideMapleMApi(
+        @Named(MapleMMoshiName) moshi : Moshi
+    ) : MapleMApi {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.MapleMBaseUrl)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(provideOkHttpClient(NexonInterceptor()))
+            .build()
+            .create(MapleMApi::class.java)
+    }
+
+    private fun provideOkHttpClient(vararg interceptor: Interceptor) : OkHttpClient =
+        OkHttpClient.Builder().run {
+            interceptor.forEach { addInterceptor(it) }
+            build()
+        }
+
+    private class NexonInterceptor : Interceptor {
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            return with(chain) {
+                val newRequest = request().newBuilder()
+                    .addHeader("x-nxopen-api-key",BuildConfig.API_KEY)
+                    .build()
+                proceed(newRequest)
+            }
+        }
+    }
+}
---
+++ core/data/src/main/java/com/hegunhee/maplemfinder/core/data/util/InjectNamedLabel.kt
@@ -0,0 +1,3 @@
+package com.hegunhee.maplemfinder.core.data.util
+
+const val MapleMMoshiName = "MapleMMoshi"
