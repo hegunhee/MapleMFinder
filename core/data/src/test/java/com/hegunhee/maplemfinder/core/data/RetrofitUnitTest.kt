49ae1d7979863a40ec02e3b9b133304ca175f1ca
hegunhee <gunhee0072@gmail.com>
2023-12-22 09:25:48 +0900

    [nexon-api] test : Ocid를 가져오는 Test

---
+++ core/data/src/test/java/com/hegunhee/maplemfinder/core/data/RetrofitUnitTest.kt
@@ -0,0 +1,58 @@
+package com.hegunhee.maplemfinder.core.data
+
+import com.hegunhee.maplemfinder.core.data.api.MapleMApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.*
+
+class RetrofitUnitTest {
+
+    private lateinit var mapleMApi : MapleMApi
+    @Before
+    fun initMapleMApi() {
+        mapleMApi = getMapleMApi()
+    }
+
+    // 정상적인 query값일경우
+    @Test
+    fun getMapleMOcidTest() {
+        runBlocking {
+            runCatching {
+                mapleMApi.getOcid(
+                    name = characterName,
+                    world = worldName
+                )
+            }.onSuccess {
+                assertEquals(requestOcid,it.id)
+            }.onFailure {
+                println(it.message)
+                assert(false)
+            }
+        }
+    }
+
+    @Test
+    fun getInvailedMapleMOcidTest() {
+        runBlocking {
+            runCatching {
+                mapleMApi.getOcid(
+                    name = invailedCharacterName,
+                    world = worldName
+                )
+            }.onSuccess {
+                assert(false)
+            }.onFailure {
+                println(it.message)
+                assertEquals(invailedQueryResponse,it.message)
+            }
+        }
+    }
+
+    private val characterName = "지존"
+    private val worldName = "아케인"
+    private val requestOcid = "9857d4a03a080b40cad682141ae3b0729c8695ba60dec42d9da8928f8ed9773c"
+
+    private val invailedCharacterName = "ㅁ나어민어ㅏㅁ너이"
+    private val invailedQueryResponse = "HTTP 400 "
+}
