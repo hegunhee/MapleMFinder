31d14d2ffc1880320d74b2d97cfc3eb76b54c9a5
hegunhee <gunhee0072@gmail.com>
2023-12-28 16:15:24 +0900

    [mainUI] build : compose-navigation 의존성 추가

--- build-logic/src/main/java/com/hegunhee/maplemfinder/build_logic/maplemfinder.android.feature.gradle.kts
+++ build-logic/src/main/java/com/hegunhee/maplemfinder/build_logic/maplemfinder.android.feature.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation(libs.findLibrary("lifecycle-viewmodel").get())
     implementation(libs.findLibrary("hilt-navigation").get())
     implementation(libs.findLibrary("activity-compose").get())
+    implementation(libs.findLibrary("compose-navigation").get())
 
     implementation(libs.findLibrary("coroutine-core").get())
 }
