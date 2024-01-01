33a426c0e484b92572f4ca2440cc4a7a672efb4a
hegunhee <gunhee0072@gmail.com>
2024-01-01 19:58:04 +0900

    [sharedPrefence] feat : 즐겨찾기 저장한 캐릭터 Ocid 추가, 수정 코드 작성

--- core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/MapleMPreferenceManager.kt
+++ core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/MapleMPreferenceManager.kt
@@ -2,6 +2,8 @@ package com.hegunhee.maplemfinder.core.data.dataSource.local
 
 import android.content.Context
 import androidx.core.content.edit
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -10,6 +12,10 @@ import javax.inject.Singleton
 class MapleMPreferenceManager @Inject constructor(@ApplicationContext context : Context) {
 
     private val prefs = context.getSharedPreferences(MapleMKey,Context.MODE_PRIVATE)
+
+    private val moshi = Moshi.Builder().build()
+
+    private val listType = Types.newParameterizedType(List::class.java, String::class.java)
 
     fun getMainOcid() : String {
         return prefs.getString(mainCharacterKey, emptyOcid) ?: emptyOcid
@@ -20,6 +26,49 @@ class MapleMPreferenceManager @Inject constructor(@ApplicationContext context : 
             putString(mainCharacterKey,ocid)
             apply()
         }
+    }
+
+    fun getLikeOcidList() : List<String> {
+        val prefsJson = prefs.getString(likeCharacterKey, emptyOcid)
+        return if(prefsJson.isNullOrBlank()) {
+            emptyList()
+        } else {
+            prefsJson.fromJson()
+        }
+    }
+
+    fun addLikeOcid(ocid : String) {
+        val ocidList = getLikeOcidList()
+        if(ocidList.contains(ocid)) {
+            return
+        }
+        (ocidList + ocid).let { addedList ->
+            prefs.edit {
+                putString(likeCharacterKey,addedList.toJson())
+                apply()
+            }
+        }
+    }
+
+    fun deleteLikeOcid(ocid : String) {
+        val ocidList = getLikeOcidList()
+        if(!ocidList.contains(ocid)) {
+            return
+        }
+        ocidList.filter { it == ocid }.let { changedList ->
+            prefs.edit {
+                putString(likeCharacterKey,changedList.toJson())
+                apply()
+            }
+        }
+    }
+
+    private fun List<String>.toJson() : String {
+        return moshi.adapter<List<String>>(listType).toJson(this)
+    }
+
+    private fun String.fromJson() : List<String> {
+        return moshi.adapter<List<String>>(listType).fromJson(this) ?: emptyList()
     }
 
     companion object {
