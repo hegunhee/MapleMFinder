5ddce5186eb03d8f0fcbad9e826dd92a075b87d3
hegunhee <gunhee0072@gmail.com>
2024-01-02 16:07:57 +0900

    [mainUI] feat : 앱의 color,typo 정보를 담는 designsystem 모듈

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -11,5 +11,6 @@ dependencies {
     implementation(project(":core:domain"))
     implementation(project(":core:model"))
     implementation(project(":core:ui"))
+    implementation(project(":core:designsystem"))
 
 }
