07a8cb79f7651e8e58691e4b3b13a16486ec78c7
hegunhee <gunhee0072@gmail.com>
2024-01-04 16:02:55 +0900

    [mainUI] feat : 즐겨찾기 메인 버튼 작성

--- core/designsystem/src/main/java/com/hegunhee/maplemfinder/core/designsystem/theme/Stroke.kt
+++ core/designsystem/src/main/java/com/hegunhee/maplemfinder/core/designsystem/theme/Stroke.kt
@@ -3,4 +3,5 @@ package com.hegunhee.maplemfinder.core.designsystem.theme
 import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.graphics.drawscope.Stroke
 
-val NormalStroke = Stroke(width = 3f, pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(20f, 10f), phase = 10f))
+val NormalDottedStroke = Stroke(width = 3f, pathEffect = PathEffect.dashPathEffect(intervals = floatArrayOf(20f, 10f), phase = 10f))
+val NormalStroke = Stroke(width = 3f)
