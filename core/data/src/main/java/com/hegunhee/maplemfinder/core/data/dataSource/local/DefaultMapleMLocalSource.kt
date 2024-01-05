a8e5dc1e5fe76f7e070a5f6c188bbc2e0cbcd996
hegunhee <gunhee0072@gmail.com>
2024-01-05 15:54:38 +0900

    [MapleM-searchUI] feat : 메이플M 서버 아이콘 추가 및 아이콘 검색 dataSource코드 작성

--- core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/DefaultMapleMLocalSource.kt
+++ core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/DefaultMapleMLocalSource.kt
@@ -1,10 +1,22 @@
 package com.hegunhee.maplemfinder.core.data.dataSource.local
 
+import com.hegunhee.maplefinder.core.model.mapleM.MapleMWorld
+import com.hegunhee.maplemfinder.core.data.R
 import javax.inject.Inject
 
 class DefaultMapleMLocalSource @Inject constructor(
     private val mapleMPreferenceManager : MapleMPreferenceManager
 ) : MapleMLocalDataSource{
+
+    private val serverList = listOf<MapleMWorld>(
+        MapleMWorld(name = "아케인",icon = R.drawable.arcane),
+        MapleMWorld(name = "크로아",icon = R.drawable.croa),
+        MapleMWorld(name = "엘리시움",icon = R.drawable.elysium),
+        MapleMWorld(name = "루나",icon = R.drawable.Luna),
+        MapleMWorld(name = "스카니아",icon = R.drawable.Scania),
+        MapleMWorld(name = "유니온",icon = R.drawable.union),
+        MapleMWorld(name = "제니스",icon = R.drawable.Zenith)
+    )
 
     override fun getMainOcid(): String {
         return mapleMPreferenceManager.getMainOcid()
@@ -12,5 +24,13 @@ class DefaultMapleMLocalSource @Inject constructor(
 
     override fun isFavoriteListEmpty(): Boolean {
         return mapleMPreferenceManager.isFavoriteListEmpty()
+    }
+
+    override fun getServerList(): List<MapleMWorld> {
+        return serverList
+    }
+
+    override fun getServerIcon(name: String): Int {
+        return serverList.find { it.name == name }?.icon ?: R.drawable.ic_default_server_mark_24
     }
 }
