b824513f4d2574408b1166c81322848cb188a914
hegunhee <gunhee0072@gmail.com>
2024-01-05 15:54:38 +0900

    [MapleM-searchUI] feat : 메이플M 서버 아이콘 추가 및 아이콘 검색 dataSource코드 작성

--- core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/MapleMLocalDataSource.kt
+++ core/data/src/main/java/com/hegunhee/maplemfinder/core/data/dataSource/local/MapleMLocalDataSource.kt
@@ -1,8 +1,14 @@
 package com.hegunhee.maplemfinder.core.data.dataSource.local
 
+import com.hegunhee.maplefinder.core.model.mapleM.MapleMWorld
+
 interface MapleMLocalDataSource {
 
     fun getMainOcid() : String
 
     fun isFavoriteListEmpty() : Boolean
+
+    fun getServerList() : List<MapleMWorld>
+
+    fun getServerIcon(name : String) : Int
 }
