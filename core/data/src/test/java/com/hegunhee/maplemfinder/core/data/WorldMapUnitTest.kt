2b9aaa5d063fcc54211e4461f875100ad4dbfc4a
hegunhee <gunhee0072@gmail.com>
2024-01-07 16:32:00 +0900

    [MapleM-searchUI] test : 월드 이름을 기준으로 이름과 아이콘을 보유하고있는 월드 객체를 반환하는지 테스트

---
+++ core/data/src/test/java/com/hegunhee/maplemfinder/core/data/WorldMapUnitTest.kt
@@ -0,0 +1,32 @@
+package com.hegunhee.maplemfinder.core.data
+
+import com.hegunhee.maplefinder.core.model.mapleM.MapleMWorld
+import com.hegunhee.maplemfinder.core.data.mapper.worldList
+import com.hegunhee.maplemfinder.core.data.mapper.worldNameToWorld
+import org.junit.Test
+
+class WorldMapUnitTest {
+
+    @Test
+    fun mapleWorldListToMapTest(){
+        val list = worldList
+        val a = list.map { worldNameToWorld(it.name) }
+        if(a == list){
+            assert(true)
+        }else{
+            assert(false)
+        }
+    }
+
+    @Test
+    fun falseWorldMapNameTest() {
+        val unknownMap = MapleMWorld(name = "Unknown",icon = R.drawable.ic_default_server_mark_24)
+        val returnMap = worldNameToWorld(unknownMap.name)
+        if(unknownMap == returnMap) {
+            assert(true)
+        }else {
+            assert(false)
+        }
+
+    }
+}
