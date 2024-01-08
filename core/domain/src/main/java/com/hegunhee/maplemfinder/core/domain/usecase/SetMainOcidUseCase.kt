f1f4f162a156e2b4cd82fff8c15239c5a7f6aa0e
hegunhee <gunhee0072@gmail.com>
2024-01-08 22:59:53 +0900

    [MapleM-searchUI] feat : 메인 ocid와 즐겨찾기 ocid를 설정하는 도메인 모듈 코드 작성

---
+++ core/domain/src/main/java/com/hegunhee/maplemfinder/core/domain/usecase/SetMainOcidUseCase.kt
@@ -0,0 +1,13 @@
+package com.hegunhee.maplemfinder.core.domain.usecase
+
+import com.hegunhee.maplemfinder.core.domain.repository.Repository
+import javax.inject.Inject
+
+class SetMainOcidUseCase @Inject constructor(
+    private val repository: Repository
+){
+
+    operator fun invoke(ocid : String) {
+        repository.setMainOcid(ocid)
+    }
+}
